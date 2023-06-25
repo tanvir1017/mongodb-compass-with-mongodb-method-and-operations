f1537c46d2ad30ff1fd0bf7bae6198cabf288ef5
tanvir1017 <tanvir.hossaindev1@gamil.com>
2023-06-25 14:37:40 +0600

    feat: 🎸 added folder structure
    added folder structure
    
    BREAKING CHANGE: 🧨 added folder structure
    
    ✅ Closes: added folder structure

--- db/mongodb/mongoose/modules/products/product.service.ts
+++ db/mongodb/mongoose/modules/products/product.service.ts
@@ -1,1 +1,11 @@
 // Mongoose builtin method for post, get, will be here. e.g products.save()
+
+/*
+export const createBookToDB = async(data:IBook):Promise<IBook> => {
+    const books = new Book(data);
+    // console.log(books)
+    // console.log(books.addFileldFuc())
+    await books.save();
+    return books; 
+}
+*/ 
--- db/mongodb/mongoose/modules/products/products.controller.ts
+++ db/mongodb/mongoose/modules/products/products.controller.ts
@@ -1,1 +1,15 @@
 // All controller function will appear here. we will call the function which is inside service.ts file as req, res
+/* export const createBook = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const data = req.body;
+  const books = await createBookToDB(data);
+
+  res.status(200).json({
+    status: "success",
+    data: books,
+  });
+};
+*/ 
